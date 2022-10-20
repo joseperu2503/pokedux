c54706c13862071b7ef6507b6b67bf77d69d6a75
joseperu2503 <51133263+joseperu2503@users.noreply.github.com>
2022-10-20 02:09:03 -0500

    console.logs

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getPokemons = async () => {
 	try {
 		const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
-		console.log(data.results);
+		// console.log(data.results);
 		return data.results;
 	} catch (error) {
 		console.error('There was an error: ', error);
